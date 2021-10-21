a2d24ebd584ba69bc9061f0a06a0bdab7b0846cc
luoshengju <luoshengju@ddky.com>
2021-10-21 10:11:59 +0800

    blog

--- blogs/app.js
+++ blogs/app.js
@@ -1,8 +1,68 @@
-import { listenAndServe } from 'https://deno.land/std@0.111.0/http/server.ts'
+const router = new Router()
 
-function handler(req) {
-    return new Response('This is a blog')
-}
+const contentType = 'application/json; charset=UTF-8'
+router
+    .get('/', context => {
+        context.response.body = `This is a Blog!  ${moment().format('YYYY-MM-DD')}`
+    })
+    .get('/api/:id', context => {
+        const { pathname } = new URL(request.url)
+        if (pathname.startsWith('/api')) {
+            const data = JSON.parse(Deno.readTextFileSync('./static/data.json'))
+            const id = context.params.id
+            console.log(`url params id => ${id}`)
+            const json = JSON.stringify({
+                message: 'First deno demo'
+            })
 
-console.log('Listening on http://localhost:8000')
-await listenAndServe(':8000', handler)
+            if (context.params && context.params.id && data[id]) {
+                if (data[id].expiryDate) {
+                    return new Response(data[id], {
+                        headers: {
+                            'content-type': contentType
+                        }
+                    })
+                } else {
+                    return new Response(
+                        { code: 0, data: {}, msg: '暂无数据' },
+                        {
+                            headers: {
+                                'content-type': contentType
+                            }
+                        }
+                    )
+                }
+            }
+        } else {
+            context.response.body = '404'
+        }
+    })
+
+const app = new Application()
+// error handler
+app.use(async (context, next) => {
+    try {
+        await next()
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+// static content
+app.use(async (context, next) => {
+    try {
+        await send(context, context.request.url.pathname, {
+            root: `${Deno.cwd()}/static`,
+            index: 'index.html'
+        })
+    } catch {
+        next()
+    }
+})
+
+const port = 9000
+console.log(`> Listening on http://localhost:${port}`)
+await app.listen({ port })
+console.log('REEEEEE')
