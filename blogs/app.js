fa8865008ed6b2b3dc4bbfbe607b028f3e832eef
luoshengju <luoshengju@ddky.com>
2021-10-21 10:46:29 +0800

    blog

--- blogs/app.js
+++ blogs/app.js
@@ -1,39 +1,39 @@
 const router = new Router()
 
-const contentType = 'application/json; charset=UTF-8'
-router
-    .get('/index', context => {
-        context.response.body = `This is a Blog!  ${moment().format('YYYY-MM-DD')}`
-    })
-    .get('/api/:id', context => {
-        const { pathname } = new URL(request.url)
-        if (pathname.startsWith('/api')) {
-            const data = JSON.parse(Deno.readTextFileSync('./static/data.json'))
-            const id = context.params.id
-            console.log(`url params id => ${id}`)
+// const contentType = 'application/json; charset=UTF-8'
+// router
+//     .get('/index', context => {
+//         context.response.body = `This is a Blog!  ${moment().format('YYYY-MM-DD')}`
+//     })
+//     .get('/api/:id', context => {
+//         const { pathname } = new URL(request.url)
+//         if (pathname.startsWith('/api')) {
+//             const data = JSON.parse(Deno.readTextFileSync('./static/data.json'))
+//             const id = context.params.id
+//             console.log(`url params id => ${id}`)
 
-            if (context.params && context.params.id && data[id]) {
-                if (data[id].expiryDate) {
-                    return new Response(data[id], {
-                        headers: {
-                            'content-type': contentType
-                        }
-                    })
-                } else {
-                    return new Response(
-                        { code: 0, data: {}, msg: '暂无数据' },
-                        {
-                            headers: {
-                                'content-type': contentType
-                            }
-                        }
-                    )
-                }
-            }
-        } else {
-            context.response.body = '404'
-        }
-    })
+//             if (context.params && context.params.id && data[id]) {
+//                 if (data[id].expiryDate) {
+//                     return new Response(data[id], {
+//                         headers: {
+//                             'content-type': contentType
+//                         }
+//                     })
+//                 } else {
+//                     return new Response(
+//                         { code: 0, data: {}, msg: '暂无数据' },
+//                         {
+//                             headers: {
+//                                 'content-type': contentType
+//                             }
+//                         }
+//                     )
+//                 }
+//             }
+//         } else {
+//             context.response.body = '404'
+//         }
+//     })
 
 const app = new Application()
 // error handler
